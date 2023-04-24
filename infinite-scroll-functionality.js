007c56e38b7e3ead18c9d98ff42aaf8c4422830f
Niki Hapunik <nikitahapunik@gmail.com>
2023-04-24 22:55:13 +0100

    - Connect the JS functionality document to the Unsplash API
    - Implement infinite scroll functionality

--- infinite-scroll-functionality.js
+++ infinite-scroll-functionality.js
@@ -1,5 +1,85 @@
-document.getElementById("search-box").addEventListener("keyup", function (event) {
+document
+  .getElementById("search-box")
+  .addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
+      searchImages(event.target.value);
+    }
+  });
+
+const accessKey = "kQaoyz53sG-uxIBAH-wpEEmd8bEYaqjtc_hjGrnxyRw";
+const apiEndpoint = "https://api.unsplash.com/search/photos";
+
+async function searchImages(query) {
+  try {
+    const response = await fetch(
+      `${apiEndpoint}?query=${query}&client_id=${accessKey}`
+    );
+    const data = await response.json();
+
+    displayImages(data.results);
+  } catch (error) {
+    console.error("Error fetching images:", error);
+  }
+}
+
+function displayImages(images) {
+  const container = document.getElementById("image-container");
+  container.innerHTML = "";
+
+  images.forEach((image) => {
+    const imgElement = document.createElement("img");
+    imgElement.src = image.urls.small;
+    imgElement.classList.add("image");
+    container.appendChild(imgElement);
+  });
+}
+let currentPage = 1;
+let isLoading = false;
+
+async function loadMoreImages(query) {
+  isLoading = true;
+  try {
+    const response = await fetch(
+      `${apiEndpoint}?query=${query}&client_id=${accessKey}&page=${currentPage}`
+    );
+    const data = await response.json();
+
+    displayImages(data.results, true);
+    isLoading = false;
+    currentPage++;
+  } catch (error) {
+    console.error("Error fetching images:", error);
+  }
+}
+function displayImages(images, append = false) {
+  const container = document.getElementById("image-container");
+
+  if (!append) {
+    container.innerHTML = "";
+  }
+
+  images.forEach((image) => {
+    const imgElement = document.createElement("img");
+    imgElement.src = image.urls.small;
+    imgElement.classList.add("image");
+    container.appendChild(imgElement);
+  });
+}
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (!isLoading && scrollTop + clientHeight >= scrollHeight - 5) {
+    const searchBox = document.getElementById("search-box");
+    loadMoreImages(searchBox.value);
+  }
+});
+
+document
+  .getElementById("search-box")
+  .addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      currentPage = 1;
       searchImages(event.target.value);
     }
   });
